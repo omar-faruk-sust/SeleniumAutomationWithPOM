41792d015f68779b624021802456256f149acb3e
Omar Faruk <omarfaruk.sust@gmail.com>
2024-06-08 02:22:10 -0400

    Automate the open new account page

--- src/test/java/com/parabank/parasoft/testcases/BaseTest.java
+++ src/test/java/com/parabank/parasoft/testcases/BaseTest.java
@@ -4,18 +4,75 @@ import com.parabank.parasoft.pages.BasePage;
 import com.parabank.parasoft.pages.Page;
 import com.parabank.parasoft.util.Common;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.firefox.FirefoxOptions;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
 
 public class BaseTest {
     WebDriver driver;
     Page page;
 
+    private Properties properties;
+
+    public BaseTest() {
+        try {
+            properties = new Properties();
+            // System.getProperty("user.dir") will give you the project root path
+            String filePath = System.getProperty("user.dir") + "/src/test/resources/config.properties";
+
+            FileInputStream inputStream = new FileInputStream(filePath);
+            properties.load(inputStream);
+        } catch (FileNotFoundException e) {
+            throw new RuntimeException(e);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
     @BeforeMethod
     public void setUpBrowser() {
-        driver = new FirefoxDriver();
-        driver.get(Common.BASE_URL);
+        String browserName = properties.getProperty("browserName");
+
+        switch (browserName) {
+            case Common.CHROME:
+                ChromeOptions options = new ChromeOptions();
+                options.setBrowserVersion("116.0.5845.111");
+                driver = new ChromeDriver(options);
+                break;
+
+            case Common.CHROME_HEADLESS:
+                driver = new ChromeDriver(
+                        (ChromeOptions) new ChromeOptions()
+                                .setBrowserVersion("116.0.5845.111")
+                                .addArguments("--headless")
+                );
+                break;
+
+            case Common.FIREFOX:
+                FirefoxOptions firefoxOptions = new FirefoxOptions();
+                driver = new FirefoxDriver(firefoxOptions);
+                break;
+
+            case Common.FIREFOX_HEADLESS:
+                driver = new FirefoxDriver(
+                        new FirefoxOptions().addArguments("--headless")
+                );
+                break;
+
+            default:
+                throw new IllegalStateException("Unexpected value: " + browserName);
+        }
+
+        driver.get(this.getBaseUrl());
         driver.manage().window().maximize();
 
         page = new BasePage(driver);
@@ -23,6 +80,18 @@ public class BaseTest {
 
     @AfterMethod
     public void tearDown() {
-        driver.quit();;
+        driver.quit();
+    }
+
+    public String getBaseUrl() {
+        return properties.getProperty("url");
+    }
+
+    public String getUsername() {
+        return properties.getProperty("username");
+    }
+
+    public String getPassword() {
+        return properties.getProperty("password");
     }
 }
--- src/test/java/com/parabank/parasoft/util/Common.java
+++ src/test/java/com/parabank/parasoft/util/Common.java
@@ -4,4 +4,10 @@ public class Common {
     public static final int IMPLICIT_WAIT_TIME = 20;
     public static final String BASE_URL = "https://parabank.parasoft.com/parabank/index.htm";
     public static final String LOGIN_TITLE = "ParaBank | Welcome | Online Banking";
+    public static final String FIREFOX = "firefox";
+    public static final String FIREFOX_HEADLESS = "firefoxHeadless";
+    public static final String CHROME = "chrome";
+    public static final String CHROME_HEADLESS = "chromeHeadless";
+    public static final String CHECKING = "CHECKING";
+    public static final String SAVINGS = "SAVINGS";
 }
